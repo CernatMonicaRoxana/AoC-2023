e4cd9432a67e160ffcd76a9b9e2c567fff50689d
CernatMonicaRoxana <cernatmonicaroxana@gmail.com>
2023-12-01 12:46:51 +0200

    Part 2

--- day01/main.py
+++ day01/main.py
@@ -1,7 +1,8 @@
 import re
+import string
 
 
-def find_digits():
+def part1():
     sumi = 0
     txt = open("input.txt", "r")
     for line in txt:
@@ -17,5 +18,45 @@ def find_digits():
     return sumi
 
 
+def part2():
+    nums = {
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        'one': 'o1e',
+        'two': 't2o',
+        'three': 't3e',
+        'four': '4',
+        'five': '5e',
+        'six': '6',
+        'seven': '7',
+        'eight': 'e8t',
+        'nine': '9',
+    }
+
+    txt = open("input.txt", "r")
+
+    res = 0
+
+    for line in txt:
+        for k, v in nums.items():
+            line = line.replace(k, v)
+
+        digits = re.findall("\d", line)
+        if len(digits) == 1:
+            res += int(digits[0] * 2)
+            # print(num)
+        else:
+            res += int(digits[0] + digits[-1])
+
+    return res
+
+
 if __name__ == '__main__':
-    print(find_digits())
+    print(part2())
