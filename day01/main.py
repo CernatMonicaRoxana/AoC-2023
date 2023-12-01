3e085da5ff43efee4be56735aedfe37653f9154f
CernatMonicaRoxana <cernatmonicaroxana@gmail.com>
2023-12-01 11:44:47 +0200

    Part 1

--- day01/main.py
+++ day01/main.py
@@ -1,4 +1,21 @@
-def read_file():
+import re
+
+
+def find_digits():
+    sumi = 0
     txt = open("input.txt", "r")
     for line in txt:
-        #TODO
+        digits = re.findall("\d", line)
+        if len(digits) == 1:
+            sumi += int(digits[0] * 2)
+            # print(num)
+        else:
+            sumi += int(digits[0] + digits[-1])
+
+    txt.close()
+
+    return sumi
+
+
+if __name__ == '__main__':
+    print(find_digits())
